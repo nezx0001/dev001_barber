818008ea9fd322080759b2a4635c1c0e70fb5669
nezx0001 <nezx0001@outlook.com>
2026-02-02 20:24:37 -0300

    Update app_routes.dart

--- lib/core/app_routes.dart
+++ lib/core/app_routes.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import '../screens/home/home_screen.dart';
+import '../screens/booking/booking_screen.dart';
+import '../models/haircut.dart';
 
 class AppRoutes {
   static const home = '/';
+  static const booking = '/booking';
 
-  static Map<String, WidgetBuilder> routes = {
-    home: (context) => const HomeScreen(),
-  };
+  static Map<String, WidgetBuilder> get routes => {
+        home: (_) => const HomeScreen(),
+        booking: (context) {
+          final haircut =
+              ModalRoute.of(context)!.settings.arguments as Haircut;
+          return BookingScreen(haircut: haircut);
+        },
+      };
 }
+
