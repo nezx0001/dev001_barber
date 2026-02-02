2f6f7dc216314fd2c6ea057158bffe5a970a44ee
nezx0001 <nezx0001@outlook.com>
2026-02-02 20:30:59 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'screens/home/home_screen.dart';
+import 'core/app_routes.dart';
+import 'core/app_theme.dart';
 
 void main() {
   runApp(const AlphaBarberApp());
@@ -11,49 +12,11 @@ class AlphaBarberApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'AlphaBarber22',
       debugShowCheckedModeBanner: false,
-      title: 'Alpha Barber',
-      theme: ThemeData(
-        useMaterial3: true,
-        scaffoldBackgroundColor: const Color(0xFF1E1E1E),
-        primaryColor: const Color(0xFF121212),
-
-        colorScheme: ColorScheme.dark(
-          primary: Color(0xFFC9A24D),
-          secondary: Color(0xFFC9A24D),
-          background: Color(0xFF1E1E1E),
-        ),
-
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Color(0xFF121212),
-          foregroundColor: Colors.white,
-          centerTitle: true,
-          elevation: 0,
-        ),
-
-        textTheme: const TextTheme(
-          titleLarge: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 22,
-          ),
-          bodyMedium: TextStyle(
-            color: Color(0xFFB0B0B0),
-            fontSize: 16,
-          ),
-        ),
-
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Color(0xFFC9A24D),
-            foregroundColor: Colors.black,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-            ),
-          ),
-        ),
-      ),
-      home: const HomeScreen(),
+      theme: AppTheme.theme,
+      initialRoute: AppRoutes.home,
+      routes: AppRoutes.routes,
     );
   }
 }
